cd49008b6b8d7f056781101d7c4c3e6f06d2742b
ADARSH ATLURI <adarshatluri@ADARSHs-MacBook-Pro.local>
2024-08-23 18:41:47 +0530

    added other tests static data

--- website/main/views.py
+++ website/main/views.py
@@ -12,6 +12,7 @@ def index(request):
     return render(request, 'main/index.html')
 
 def cardiac(request):
+    print("Entered Cardiac view")
     abnormaltests=None
     normaltests=None
     if request.user.id is not None:
@@ -23,15 +24,21 @@ def cardiac(request):
 
 
 def test_detail(request,pathparameter):
-    if pathparameter=="triglicerides":
-        testdetaildata=None
-        if request.user.id is not None:
-            testdetaildata = settestdetail_data()
-        return render(request, 'main/testdetail.html',{
-            "testdetaildata":testdetaildata
-        })
-    else:
+    print("Entered test_detail view")
+    print(pathparameter)
+    testdetaildata=None
+    recenttests=None
+    if request.user.id is not None:
+        testdetaildata,recenttests = settestdetail_data(testdetaildata,recenttests,pathparameter)
+    if testdetaildata==None:
+        print("Entered test_detail view->redirect view")
         return redirect(pathparameter)
+    else:
+        return render(request, 'main/testdetail.html',{
+            "testdetaildata":testdetaildata,
+            "recenttests":recenttests
+        })
+
 
 
 def home(request):
@@ -215,126 +222,131 @@ def setcardiacpage_data():
             "refinterval":"Fasting 30-149",
             "date" : "May 25th, 2024",
             "status" : "Normal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+            "detaillink":"triglicerides",
         },
         "2" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
+            "name" : "Serum H.D.L. Cholesterol",
+            "result":53,
             "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
+            "refinterval":"40 - 59",
             "date" : "May 25th, 2024",
             "status" : "Normal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+            "detaillink":"hdl",
         },
         "3" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
+            "name" : "Serum V.L.D.L. Cholesterol",
+            "result":14,
             "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
+            "refinterval":"10 - 35",
             "date" : "May 25th, 2024",
             "status" : "Normal",
             "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+            "detaillink":"vldl",
         },
         "4" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
-            "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
+            "name" : "Plasma Sugar Fasting",
+            "result":79,
+            "unit":"mgs/dl",
+            "refinterval":"Fasting 70 - 110",
             "date" : "May 25th, 2024",
             "status" : "Normal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+            "detaillink":"plasmasugarfasting",
         },
     }
    abnormaltests = {
         "1" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
+            "name" : "Serum Cholesterol",
+            "result":221,
             "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
+            "refinterval":"Less than 200",
             "date" : "May 25th, 2024",
             "status" : "Abnormal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+            "detaillink":"serumcholestrol"
         },
         "2" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
+            "name" : "Serum L.D.L. Cholesterol",
+            "result":154,
             "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
+            "refinterval":"Less than 130",
             "date" : "May 25th, 2024",
             "status" : "Abnormal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
-        },
+            "detaillink":"serumldlcholestrol"
+            },
         "3" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
+            "name" : "Serum Urea",
+            "result":44,
             "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
+            "refinterval":"17 - 43",
             "date" : "May 25th, 2024",
             "status" : "Abnormal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+            "detaillink":"serumurea"
         },
         "4" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
-            "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
+            "name" : "Glycosylated Haemoglobin (HbA1c)",
+            "result":5.7,
+            "unit":"%",
+            "refinterval":"Below 5.7",
             "date" : "May 25th, 2024",
             "status" : "Abnormal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+            "detaillink":"glycosylatedhaemoglobin"
         },
     }
    return normaltests,abnormaltests
 
-def settestdetail_data():
-   testdetaildata = {
-        "1" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
-            "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
-            "date" : "May 25th, 2024",
-            "status" : "Normal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
-        },
-        "2" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
-            "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
-            "date" : "May 25th, 2024",
-            "status" : "Normal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
-        },
-        "3" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
-            "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
-            "date" : "May 25th, 2024",
-            "status" : "Normal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
-        },
-        "4" : {
-            "name" : "Serum Triglycerides",
-            "result":68,
-            "unit":"mg/dl",
-            "refinterval":"Fasting 30-149",
-            "date" : "May 25th, 2024",
-            "status" : "Normal",
-            "identificationnumber":50450206,
-            "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
-        },
-    }
-   return testdetaildata
+def settestdetail_data(testdetaildata,recenttests,pathparameter):
+   if(pathparameter=="triglicerides"):
+        testdetaildata = {
+                    "name" : "Serum Triglycerides",
+                    "result":68,
+                    "unit":"mg/dl",
+                    "refinterval":"Optimal - 30-149 ,Boarder-line High 150 – 199 mg/dL ,High 200 - 399 mg/dL ,Very High > 400 mg/dL",
+                    "date" : "May 25th, 2024",
+                    "status" : "Normal",
+                    "methodology":"Lipase GK: GPO POD",
+                    "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+        }
+        recenttests = {
+                "1" : {
+                    "name" : "Serum Triglycerides",
+                    "result":68,
+                    "unit":"mg/dl",
+                    "refinterval":"Fasting 30-149",
+                    "date" : "May 25th, 2024",
+                    "status" : "Normal",
+                    "identificationnumber":50450206,
+                    "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease .",
+                    "detaillink":"triglicerides"
+                },
+                "2" : {
+                    "name" : "Serum H.D.L. Cholesterol",
+                    "result":53,
+                    "unit":"mg/dl",
+                    "refinterval":"40-59",
+                    "date" : "May 25th, 2024",
+                    "status" : "Normal",
+                    "identificationnumber":50450206,
+                    "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+                },
+                "3" : {
+                    "name" : "Serum Triglycerides",
+                    "result":68,
+                    "unit":"mg/dl",
+                    "refinterval":"Fasting 30-149",
+                    "date" : "May 25th, 2024",
+                    "status" : "Normal",
+                    "identificationnumber":50450206,
+                    "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+                },
+                "4" : {
+                    "name" : "Serum Triglycerides",
+                    "result":68,
+                    "unit":"mg/dl",
+                    "refinterval":"Fasting 30-149",
+                    "date" : "May 25th, 2024",
+                    "status" : "Normal",
+                    "identificationnumber":50450206,
+                    "description":"Triglycerides are an important measure of heart health. High triglycerides may contribute to hardening of the arteries or thickening of the artery walls (arteriosclerosis) — which increases the risk of stroke, heart attack and heart disease ."
+                },
+        }
+   return testdetaildata,recenttests
 
